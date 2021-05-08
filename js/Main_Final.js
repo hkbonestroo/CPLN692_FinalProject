f85a5d6a77d434898867158a746cbeeda87a0a34
hkb1414 <43580459+hkbonestroo@users.noreply.github.com>
2021-05-07 23:31:09 -0400

    finished framework
    just need to add writing

--- js/Main_Final.js
+++ js/Main_Final.js
@@ -3,7 +3,7 @@ Leaflet Configuration
 ===================== */
 var map = L.map('map', {
   center: [39.945389, -75.165166],
-  zoom: 16
+  zoom: 15
 });
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -13,6 +13,10 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
   ext: 'png'
 }).addTo(map);
 
+
+$( ".map1" ).hide()
+$( ".map2" ).hide()
+$( ".map3" ).hide()
 $( "#map" ).hide()
 $("#returnDoor").hide()
 $( ".pagination" ).hide()
@@ -43,8 +47,10 @@ var page1 = function(){
   $(".page6").hide()
   $(".page7").hide()
   $( "#map" ).hide()
+  $( ".map1" ).hide()
   $( "#returnDoor" ).hide()
   $( ".pagination" ).show()
+  $( ".leaflet-container img.leaflet-image-layer" ).remove()
 }
 
 //page 2 elements
@@ -82,8 +88,11 @@ var page4 = function(){
   $(".page5").hide()
   $(".page6").hide()
   $(".page7").hide()
+  $( "#map" ).hide()
+  $( ".map2" ).hide()
   $( "#returnDoor" ).hide()
   $( ".pagination" ).show()
+  $( ".leaflet-container img.leaflet-image-layer" ).remove()
 }
 
 //page 5 elements
@@ -95,8 +104,11 @@ var page5 = function(){
   $(".page5").show()
   $(".page6").hide()
   $(".page7").hide()
+  $( "#map" ).hide()
+  $( ".map3" ).hide()
   $( "#returnDoor" ).hide()
   $( ".pagination" ).show()
+  $( ".leaflet-container img.leaflet-image-layer" ).remove()
 }
 
 //page 6 elements
@@ -129,86 +141,72 @@ var page7 = function(){
 var factsPopUp1a =function() {
   var popup = document.getElementById("myPopup1a");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp1b =function() {
   var popup = document.getElementById("myPopup1b");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp1c =function() {
   var popup = document.getElementById("myPopup1c");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 
 var factsPopUp1d =function() {
   var popup = document.getElementById("myPopup1d");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp1e =function() {
   var popup = document.getElementById("myPopup1e");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp2a =function() {
   var popup = document.getElementById("myPopup2a");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp2b =function() {
   var popup = document.getElementById("myPopup2b");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp2c =function() {
   var popup = document.getElementById("myPopup2c");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp2d =function() {
   var popup = document.getElementById("myPopup2d");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp3a =function() {
   var popup = document.getElementById("myPopup3a");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp4a =function() {
   var popup = document.getElementById("myPopup4a");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp4b =function() {
   var popup = document.getElementById("myPopup4b");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp4c =function() {
   var popup = document.getElementById("myPopup4c");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp4d =function() {
   var popup = document.getElementById("myPopup4d");
   popup.classList.toggle("show")
-  console.log("Hello");
 }
 
 var factsPopUp4e =function() {
@@ -217,9 +215,39 @@ var factsPopUp4e =function() {
   console.log("Hello");
 }
 
+var factsPopUp5a =function() {
+  var popup = document.getElementById("myPopup5a");
+  popup.classList.toggle("show")
+}
 
+var factsPopUp5b =function() {
+  var popup = document.getElementById("myPopup5b");
+  popup.classList.toggle("show")
+}
+
+var factsPopUp5c =function() {
+  var popup = document.getElementById("myPopup5c");
+  popup.classList.toggle("show")
+}
+
+
+var factsPopUp5d =function() {
+  var popup = document.getElementById("myPopup5d");
+  popup.classList.toggle("show")
+}
+
+var factsPopUp6a =function() {
+  var popup = document.getElementById("myPopup6a");
+  popup.classList.toggle("show")
+}
+
+
+var factsPopUp6b =function() {
+  var popup = document.getElementById("myPopup6b");
+  popup.classList.toggle("show")
+}
 //map function
-var mapOpen = function() {
+var mapOpen1 = function() {
   $(".page1").hide()
   $(".page2").hide()
   $(".page3").hide()
@@ -229,8 +257,10 @@ var mapOpen = function() {
   $(".page7").hide()
   $(".pagination").hide()
   $( "#map" ).show()
+  $( ".map1" ).show()
+
   $("#returnDoor").show()
-  
+  map.invalidateSize()
 //39.941579514468785,-75.15235304832458
 //39.94168439324544,-75.18299996852873
 //39.9500166010947,-75.15238523483276
@@ -242,13 +272,115 @@ var mapOpen = function() {
   markers.addTo(map)
 }
 
+var mapOpen2 = function() {
+
+  $(".page1").hide()
+  $(".page2").hide()
+  $(".page3").hide()
+  $(".page4").hide()
+  $(".page5").hide()
+  $(".page6").hide()
+  $(".page7").hide()
+  $(".pagination").hide()
+  $( "#map" ).show()
+  $( ".map2" ).show()
+  $("#returnDoor").show()
+  map.invalidateSize()
+  
+//39.941579514468785,-75.15235304832458
+//39.94168439324544,-75.18299996852873
+//39.9500166010947,-75.15238523483276
+  var imageUrl = "images/jmbrewer2.png"
+    imageBounds = [[39.95251997218897, -75.17346996102333
+      ], [39.93628601211787,-75.17466954784393
+        ], [39.935317343242325,-75.13714492321014
+          ],[39.95176023187717,-75.13776404953003
+      ]];
+  L.imageOverlay(imageUrl, imageBounds,{opacity: 0.4}).addTo(map);
+  markers= L.marker([39.94347656161361,-75.15382155776024]).bindPopup("424 S 7th St")
+  markers.addTo(map)
+}
+
+var mapOpen3 = function() {
+
+  $(".page1").hide()
+  $(".page2").hide()
+  $(".page3").hide()
+  $(".page4").hide()
+  $(".page5").hide()
+  $(".page6").hide()
+  $(".page7").hide()
+  $(".pagination").hide()
+  $( "#map" ).show()
+  $( ".map3" ).show()
+  $("#returnDoor").show()
+  map.invalidateSize()
+  
+//39.941579514468785,-75.15235304832458
+//39.94168439324544,-75.18299996852873
+//39.9500166010947,-75.15238523483276
+  var imageUrl = "images/1962landuse.png"
+    imageBounds = [[39.94769717428357,-75.16264200210571
+      ], [39.94702271626765,  -75.13932824134827
+        ], [39.93690916128112, -75.1505162296295
+          
+          
+          
+          
+          
+        
+          ],[39.93679810541011,-75.16264200210571
+      ]];
+  L.imageOverlay(imageUrl, imageBounds,{opacity: 0.4}).addTo(map);
+  markers= L.marker([39.94347656161361,-75.15382155776024]).bindPopup("424 S 7th St")
+  markers.addTo(map)
+}
+
+//function to go to next page
+var nextPage = function() {
+if ($(".page1").is(':visible')) {page2()} 
+else if ($(".page2").is(':visible'))
+{ page3()}
+else if ($(".page3").is(':visible'))
+{ page4()}
+else if ($(".page4").is(':visible'))
+{ page5()}
+else if ($(".page5").is(':visible'))
+{ page6()}
+else if ($(".page6").is(':visible'))
+{$('#pagination .last').prop("disabled",true)}
+}
+
+//function to go to next page
+var previousPage = function() {
+  if ($(".page6").is(':visible')) {page5()} 
+  else if ($(".page5").is(':visible'))
+  { page4()}
+  else if ($(".page4").is(':visible'))
+  { page3()}
+  else if ($(".page3").is(':visible'))
+  { page2()}
+  else if ($(".page2").is(':visible'))
+  { page1()}
+  else if ($(".page1").is(':visible'))
+  {$('#pagination .first').prop("disabled",true)}
+  }
+
+//function to go to previous page
+var prevPage = function(){
+  tearDown()
+  var prevPage =currentPage -1
+  currentPage = prevPage
+  buildPage(slides[prevPage])
+}
 
 $(".door").click(hidehouse)
 
-$("#door1").click(mapOpen)
+$("#door1").click(mapOpen1)
 
+$("#door2").click(mapOpen2)
 
-
+$("#door3").click(mapOpen3)
 $(document).ready(function(){
   $('[data-toggle="popover"]').popover();   
 });
